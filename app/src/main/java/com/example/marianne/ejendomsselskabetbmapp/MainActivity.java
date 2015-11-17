9044e2aa63f4c68a6d109ebd7b29b36569f5818d
msuhl <gate.nielsen@gmail.com>
2015-11-17 10:57:43 +0100

    popup

--- app/src/main/java/com/example/marianne/ejendomsselskabetbmapp/MainActivity.java
+++ app/src/main/java/com/example/marianne/ejendomsselskabetbmapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.marianne.ejendomsselskabetbmapp;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,18 +14,26 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Calendar;
+
 
 import java.sql.Date;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     MyDBHandler myDBHandler;
     TextView txt;
-    EditText taskDescrip, roomNr, address;
+    EditText taskDescrip, roomNr, address, Donedate;
     CheckBox acquisi, scheduled;
     DatePicker date;
     Button btngemData;
+    //datepickerdialog
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
+    private SimpleDateFormat dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +47,38 @@ public class MainActivity extends AppCompatActivity {
         address = (EditText)findViewById(R.id.etxAddress);
         acquisi = (CheckBox)findViewById(R.id.chbAcquisition);
         scheduled = (CheckBox)findViewById(R.id.chbScheduled);
-        date = (DatePicker)findViewById(R.id.dtpDate);
         btngemData = (Button)findViewById(R.id.btnSaveTask);
+        Donedate = (EditText) findViewById(R.id.EtxfromDate);
+        Donedate.requestFocus();
 
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
+        setDateTimeField();
 //        printDatabase();
         addData();
+
+    }
+
+    //Datepickerdialog start
+
+    private void setDateTimeField() {
+
+        //sæt onclicklistener, så man kan fange "klikket"
+        Donedate.setOnClickListener(this);
+
+        //lav en calendar instance, så vi dafs dato til at sætte datepickerdialog til ved åbning
+        Calendar newCalendar = Calendar.getInstance();
+        //opret en ny datepickerdialog med en OndateSetListener (kalder tilbage når man vælger en dato)
+        fromDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            //modtager callback fra datePickerDialog
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+//                henter dato/tid fra calendar objektet og formaterer det til tekst og sætter det ind i vores editText
+                Donedate.setText(dateFormatter.format(newDate.getTime()));
+            }
+            //sætter startdato for datepickerdialog(hvilken dato en viser når man åbner den)
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
     }
 
@@ -52,10 +88,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
 
-                        Date dato = new Date(date.getYear()-1900,date.getMonth(),date.getDayOfMonth());
+                        Date dato = new Date(date.getYear() - 1900, date.getMonth(), date.getDayOfMonth());
                         //String selectedDate = DateFormat.getDateInstance().format(date.getYear());
-                        String selectedDate = date.getDayOfMonth() + "/" + (date.getMonth()+1) + "/" + date.getYear();
-                        Log.d("Valgte år er: ", selectedDate +" "+ dato.toString());
+                        String selectedDate = date.getDayOfMonth() + "/" + (date.getMonth() + 1) + "/" + date.getYear();
+                        Log.d("Valgte år er: ", selectedDate + " " + dato.toString());
                         Inspectionlist myList = new Inspectionlist(selectedDate,
                                 roomNr.getText().toString(),
                                 address.getText().toString(),
@@ -72,8 +108,7 @@ public class MainActivity extends AppCompatActivity {
                             address.setText("");
                             acquisi.setChecked(false);
                             scheduled.setChecked(false);
-                        }
-                        else
+                        } else
                             Toast.makeText(MainActivity.this, "Data not Inserted", Toast.LENGTH_LONG).show();
                     }
                 }
@@ -104,5 +139,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        fromDatePickerDialog.show();
     }
 }
